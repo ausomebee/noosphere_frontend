9732eac4237ef873204f2a4d03d729bdbe98b896
nobledeveloper01 <nobleconcepts22@gmail.com>
2026-03-30 21:18:01 +0100

    corrections

--- tenant/src/Pages/Scheduler/SchdedulerSubs/Calendar.jsx
+++ tenant/src/Pages/Scheduler/SchdedulerSubs/Calendar.jsx
@@ -202,6 +202,7 @@ const Calendar = () => {
       }
       setStaff(stf);
 
+      
       // All roles fetch all appointments; filter visibility is controlled by permissions
       let allAppts = [];
       try {
